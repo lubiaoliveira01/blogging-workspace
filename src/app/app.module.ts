d8d69ebc2dad67310408d6d698956967a4234f7e
Lúbia Oliveira <lubia.oliveira01@gmail.com>
2024-02-25 18:42:21 -0300

    Criando e adicionando componente de section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
